bd0a63397e597981ae93ce3ef2c0cc2c928f724c
ikyxyzu <107379438+ikyxyzu@users.noreply.github.com>
2022-06-21 12:04:04 +0700

    Create main.js

---
+++ app/main.js
@@ -0,0 +1,29 @@
+var express = require('express');
+const axios = require('axios').default;
+var router = express.Router()
+
+router.get('/nulis', async function (req, res, next) {
+  res.render('index', {
+    title:
+      'Nulis Online - ikyxyzu',
+    my_url: "https://nulis-ikyxyzu.vercel.app",
+    desc: "Lagi mager nulis? mending Nulis Online aja, dijamin tangan gak bakalan pegel! aowkaowkokw",
+    author: "ZaaDev"
+  })
+})
+
+router.get('/', function (req, res) {
+  res.render('home', {
+    title:
+      'Nulis Online - ikyxyzu',
+    my_url: "https://nulis-ikyxyzu.vercel.app",
+    desc: "Lagi mager nulis? mending Nulis Online aja, dijamin tangan gak bakalan pegel! aowkaowkokw",
+    author: "ikyxyzu"
+  })
+})
+
+router.get('*', function (req, res) {
+  res.render('error')
+})
+// tes
+module.exports = router
